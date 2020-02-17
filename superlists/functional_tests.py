67811329174dbefddcaf8a7eb717a4b8405fb6d6
ElElefante <wfberlin@gmail.com>
2020-02-17 10:27:20 +0100

    unittest module introduced

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
+import unittest
 
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        # Ensure that page is loaded before test starts (may be more then 3 secs)
+        self.browser.implicitly_wait(3)
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
+    def test_can_start_and_retrieve_it_later(self):
+        # User starts the application
+        self.browser.get('http://localhost:8000')
+        # Did the desired application start correctly?
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the tests')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
