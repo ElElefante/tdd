4ee640062b2d5e6ffb0fed0d6e2fa6556e900dea
ElElefante <wfberlin@gmail.com>
2020-02-19 10:11:21 +0100

    added new funtional test, input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,6 +16,24 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # Did the desired application start correctly?
         self.assertIn('To-Do',self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # Enter To-Do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.__getattribute__('placeholder'),
+            'Enter a To-Do item'
+        )
+        # Typing an item
+        inputbox.send_keys('Buy peacock feathers')
+        # Terminate by pressing Enter
+        inputbox.send_keys(Keys.Enter)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         self.fail('Finish the tests')
 
 if __name__ == '__main__':
